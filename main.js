42a4ba80639b0359dea77af32e4699ff75b8e868
ernellveraldi <ernellveraldi@gmail.com>
2023-10-28 12:09:51 -0500

    add demographics

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstName= "ernell"
+const lastName= "veraldi"
+const age = 57
+
+console.log(`my first name is ${firstName} and my last name is ${lastName}`)
+console.log("and my age is ",age)
